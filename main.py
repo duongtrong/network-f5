647c0f566f37499ba6a20c8577e576a83e01475c
Dương Trọng <duongtrong@192.168.0.103>
2024-07-26 23:00:50 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from service.f5gtm import get_access_token
+from service.f5gtm import config_mgmt_f5system
+from service.f5gtm import save_sys_config
+import urllib3
+urllib3.disable_warnings()
+
+username = 'admin'
+password = 'Tcb12345'
+hostname = '13.212.238.210:8443'
+
+def get_access_token_method():
+    try:
+        get_access_token_result = get_access_token(
+            username=username,
+            password=password,
+            host=hostname)
+        if get_access_token_result['status'] == 200:
+            token = get_access_token_result['token']
+            return {'status': 200, 'token': token}
+        else:
+            return {'status': 'Bad Request', 'msg': 'ERROR'}
+    except Exception as e:
+        return {'status': 'Bad Request', 'msg': 'Exception: {}'.format(str(e))}
+# end try
+
+if __name__ == "__main__":
+    result_token = get_access_token_method()
+    print(result_token['token'])
+    configF5 = config_mgmt_f5system(username=username, password=password, timezone="Asia/Saigon", host=hostname)
+    print(configF5)
+    # state = save_sys_config(token=result_token["token"], host=hostname)
+    # print(state)
---
+++ service/f5gtm.py
@@ -0,0 +1,73 @@
+import requests 
+from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
+
+def get_access_token(username, password, host):
+    """
+        Username: username
+        Password: password
+        Host: host
+    """
+    url = "https://{}/mgmt/shared/authn/login".format(host)
+    
+    data = {
+        "username": username,
+        "password": password,
+        "loginProvideName": "tmos" 
+    }
+    payload = json.dumps(data)
+    try:
+        response = requests.post(url=url, data=payload, verify=False)
+        if response.status_code == 200:
+            token = json.loads(response.text)['token']['token']
+            return {'status': 200, 'token': token}
+        else:
+            return {'status': 'Bad Request', 'msg': response.text}
+    except Exception as e:
+        return {'status': 400, 'msg': str(e)}
+
+def config_mgmt_f5system(username, password, timezone, host):
+    """
+        username
+        password
+        timeZone
+        hostname
+    """
+    url = "https://{}/mgmt/tm/cm/device/f5test".format(host)
+    data = {
+        "timeZone": timezone
+    }
+    payload = json.dumps(data)
+    try:
+        response = requests.put(url=url, auth=(username, password), verify=False, data=payload)
+        if response.status_code == 200:
+            print("Insert success")
+            return {"status": 200, "msg": "Successfully"}
+        else:
+            print("Insert fail")
+            return {"status": 400, "msg": response.text}
+    except Exception as e:
+        return { "status": 400, "msg": str(e)}
+
+def save_sys_config(token, host):
+    """
+        token
+        hostname
+    """
+    url = "https://{}/mgmt/tm/sys/config".format(host)
+    headers = {"X-F5-Auth-token": token}
+    data = {
+        "command": "save"
+    }
+    payload = json.dumps(data)
+    try:
+        response = requests.post(url=url, headers=headers, verify=False, data=payload)
+        if response.status_code == 200:
+            print("Insert success")
+            return {"status": "OK"}
+        else:
+            print("Insert fail")
+            return {"status": "NOK", "msg": response.text}
+    except Exception as e:
+        return { "status": 400, "msg": str(e)}
